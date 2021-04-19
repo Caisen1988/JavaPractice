62ced914d9aca33cc044612537b235b19f66f993
neo_wei <neo_wei@keyoutech.com>
2021-04-19 16:49:50 +0800

    map list 2021-4-19

--- src/com/company/findScore.java
+++ src/com/company/findScore.java
@@ -1,4 +1,16 @@
 package com.company;
 
+import java.util.List;
+import java.util.Map;
+
 public class findScore {
+    List<Student> list;
+    Map<String, Integer> cache;
+
+    /**
+     * 根据name查找score，找到返回score，未找到返回-1
+     */
+    public void getScoreByStudentName(String name) {
+
+    }
 }
